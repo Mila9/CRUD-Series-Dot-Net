5fb9ef3b548af1fc02d3d73bd57463c6199c6582
Mila9 <70359104+Mila9@users.noreply.github.com>
2021-09-23 23:01:38 -0300

    Adicionando projeto

---
+++ Dio.Series/Excecao/Excecao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Series
+{
+    class Excecao: Validacao
+    {
+        //private static final long serialVersionUID = 1L;
+        public Excecao(String msg) => super(msg);
+
+        private void super(string msg)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal string getMessage()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
+
---
+++ Dio.Series/Excecao/Validacao.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dio.Series
+{
+    public class Validacao
+    {
+		public bool ValidaTitulo(string titulo,List<Serie> lista)
+		{
+			foreach (var tituloSerie in lista)
+			{
+
+				if (!tituloSerie.retornaTitulo().Trim().Equals(titulo.Trim()))
+				{
+					Console.WriteLine("Titulo valido");
+					lista.Add(tituloSerie);
+				
+				}
+				else
+				{
+					Console.WriteLine("Insira um titulo valido");
+					return false;
+				}
+
+			}
+			return true;
+		}
+	}
+}
